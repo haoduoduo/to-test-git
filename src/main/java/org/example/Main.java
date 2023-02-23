a4b90b75caab961d3133a2fca32cc70cec2a0c8f
haoduoduo <55751360+haoduoduo@users.noreply.github.com>
2023-02-23 20:51:24 +0800

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,5 +9,6 @@ public class Main {
         System.out.println("master update");
         System.out.println("xiezhihao update");
         System.out.println("master push test");
+        System.out.println("master pull test");
     }
 }
