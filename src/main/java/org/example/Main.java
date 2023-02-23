ec5bbcdd17c2a56cae839101d72585080160ea05
xiezhihao <2417479065@qq.com>
2023-02-23 20:27:54 +0800

    master update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Hello world haoduoduo!");
         System.out.println("Hello git by haoduoduo!");
         System.out.println("Hello git by xiezhihao!");
+        System.out.println("master update");
     }
 }
